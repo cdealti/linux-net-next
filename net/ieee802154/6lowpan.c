f76002af1a0863475aeab586a541ab1a45fa8e5b
Alexander Aring <alex.aring@gmail.com>
2013-10-07 23:13:57 +0200

    6lowpan: udp use subtraction on both conditions
    Cleanup code to handle both calculation in the same way.

--- net/ieee802154/6lowpan.c
+++ net/ieee802154/6lowpan.c
@@ -335,7 +335,7 @@ lowpan_compress_udp_header(u8 **hc06_ptr, struct sk_buff *skb)
 		**hc06_ptr = LOWPAN_NHC_UDP_CS_P_11;
 		*(*hc06_ptr + 1) = /* subtraction is faster */
 		   (u8)((ntohs(uh->dest) - LOWPAN_NHC_UDP_4BIT_PORT) +
-		       ((ntohs(uh->source) & LOWPAN_NHC_UDP_4BIT_PORT) << 4));
+		       ((ntohs(uh->source) - LOWPAN_NHC_UDP_4BIT_PORT) << 4));
 		*hc06_ptr += 2;
 	} else if ((ntohs(uh->dest) & LOWPAN_NHC_UDP_8BIT_MASK) ==
 			LOWPAN_NHC_UDP_8BIT_PORT) {
